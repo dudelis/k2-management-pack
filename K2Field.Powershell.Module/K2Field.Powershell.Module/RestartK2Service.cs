586642c731384574138a95e964779b284844e8c8
Konstantin Fukszon <fukszon.kostya@gmail.com>
2017-12-30 06:25:24 -0800

    #4 - Added New-K2ServiceType cmdlet

---
+++ K2Field.Powershell.Module/K2Field.Powershell.Module/RestartK2Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.ServiceProcess;
+using System.Text;
+
+namespace K2Field.Powershell.Module
+{
+    [Cmdlet("Restart", "K2Service")]
+    public class RestartK2Service : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            ModuleHelper.RestartK2Service();
+        }
+
+    }
+}
